39a1692ff255290b530ec61b52f3f556fa790036
Cesar Bess <cesar.bess@gmail.com>
2019-03-02 15:37:01 -0300

    Add extension to safely access item on collection

--- KeyboardAdjustable/Classes/Extensions/Collection+Extensions.swift
+++ KeyboardAdjustable/Classes/Extensions/Collection+Extensions.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+public extension Collection {
+
+    /// Returns the element at the specified index if it is within bounds, otherwise nil.
+    public subscript (safe index: Index) -> Element? {
+        return indices.contains(index) ? self[index] : nil
+    }
+}
